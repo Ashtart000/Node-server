8a1f271d3c5abdd1eb78ad468f170a81b250fe13
Anna Lednyova <ashtart@gmail.com>
2023-08-14 21:19:52 +0300

    server response HTML

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const fs = require('fs/promises');
 
 const PORT = 5000;
 
 const requestListener = (req, resp) => {
-    const {url, method} = req;
-    console.log(url, method)
+    const {url} = req;
+
+    if(url === '/index.html') {
+        fs.readFile('./index.html', 'utf-8')
+        .then((data) => {
+            resp.end(data)
+        })
+    } else {
+        resp.statusCode = 404;
+        resp.end();
+    }   
 }
 
 const server = http.createServer(requestListener);
